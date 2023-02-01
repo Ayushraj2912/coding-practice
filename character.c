c2c80f96001f5ebebae2b4b07f9770f3bf692bed
Ayush Raj <ayushr2912btechcse2025@kccitm.edu.in>
2023-02-01 15:54:46 +0530

    character in c

---
+++ character.c
@@ -0,0 +1,23 @@
+// to find if a character entered by user is upper case or not
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("enter character : ");
+    scanf("%c", &ch);
+    if (ch >= 'A' && ch <= 'Z')
+
+    {
+        printf("Upper case\n");
+    }
+    else if (ch >= 'a' && ch >= 'z')
+
+    {
+        printf("Lower case \n");
+    }
+    else
+    {
+        printf("not english letter\n");
+    }
+    return 0;
+}
